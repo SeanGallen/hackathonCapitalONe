f938b9b885640b17da1134553354c027ba87f6d9
Sean <sjgallen@gmail.com>
2017-03-25 19:04:41 -0500

    added intent-schema

---
+++ speech-assets/intent-schema.js
@@ -0,0 +1,27 @@
+{
+  "intents": [
+  {
+    "intent": "OfferMeCapitalOne",
+    "slots": []
+  },
+  {
+    "intent": "BestCreditCards",
+    "slots": []
+  },
+  {
+    "intent": "AMAZON.YesIntent"
+  },
+  {
+    "intent": "AMAZON.NoIntent"
+  },
+  {
+    "intent": "AMAZON.HelpIntent"
+  },
+  {
+    "intent": "AMAZON.StopIntent"
+  },
+  {
+    "intent": "AMAZON.CancelIntent"
+  }
+  ]
+}
